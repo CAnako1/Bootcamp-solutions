b3411862fa78f9ed6477b656ff5efbcb07320312
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2022-11-10 00:08:02 +0300

    Head fucked by logic

--- 04-javascript-module/01-syntax-lesson/activities/09-Stu-Conditional-Statements/starter/script.js
+++ 04-javascript-module/01-syntax-lesson/activities/09-Stu-Conditional-Statements/starter/script.js
@@ -4,3 +4,19 @@ var expression1 = (x < 25);
 var expression2 = (x > 50);
 
 // Write Your JavaScript Code Here
+
+// if (condition to check if we should run our code) {
+// -- IF the conditions resulted to true, then we run the code innside of the curly brakcets   
+//}
+
+if (expression1 && expression2) {
+    console.log("True ✅ True ✅");
+} else if (expression1) {
+    console.log("True ✅ False ❌");
+} else if (expression2) {
+    console.log( "False ❌ True ✅");
+} else {
+    console.log("False ❌ False ❌");
+}
+
+
--- 04-javascript-module/02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
+++ 04-javascript-module/02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
@@ -1,1 +1,57 @@
 // WRITE YOUR CODE HERE
+
+// var lastItem = student[student.length -1];
+// prints out the number of students starting from 0
+// console.log(student.length)
+
+
+var student = ['kevin','marta','kieran','adam','hugh','sarah','jane','fred','evie'];
+var welcomeQuote = 'Welcome to the class'
+var spacer = ' '
+var original = student[0];
+
+
+//console log total number of item in an array
+console.log(student.length);
+
+
+console.log(welcomeQuote + spacer + student[0]);
+console.log(welcomeQuote + spacer + student[1]);
+console.log(welcomeQuote + spacer + student[2]);
+console.log(welcomeQuote + spacer + student[3]);
+
+student[0] = 'dave';
+
+console.log(student)
+
+if (student[0] != original) {
+    console.log(student[0] + spacer + 'is in class')
+}
+
+
+
+
+// console.log('Welcome to the class ' + student[0]);
+
+// student[0] = 'kevin'
+
+// console.log('welcome to the class ' + student[5]);
+// console.log('welcome to the class ' + student[1]);
+// console.log('welcome to the class ' + student[2]);
+
+// if (student[0] === 'dave') {
+//     console.log(true)
+// }
+
+
+
+
+
+
+
+//student[0] = 'tom';
+
+// student.pop();
+// student.shift()
+
+// console.log (student[student.length -1]);
